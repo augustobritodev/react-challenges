8417736c6e0c0ea6842ac94d15a80a0c74ab11bf
Augusto Brito <augustobritodev@icloud.com>
2023-07-08 20:45:42 -0300

    chore: add main game

--- src/pages/GuessTheColor.jsx
+++ src/pages/GuessTheColor.jsx
@@ -1,7 +1,47 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+function getRandomHex() {
+  const digits = '0123456789ABCDEF'
+
+  let color = new Array(6)
+    .fill('')
+    .map(() => digits[Math.floor(Math.random() * digits.length)])
+    .join('')
+
+  return `#${color}`
+}
+
 export default function GuessTheColor() {
+  const [won, setWon] = useState(false)
+
+  const colors = new Array(3).fill('').map(() => getRandomHex())
+  const color = colors[Math.floor(Math.random() * colors.length)]
+
+  function handleOnClick(event) {
+    const target = event.target
+    setWon(target.innerText === color)
+  }
+
+  if (won) {
+    return (
+      <div className='flex h-screen'>
+        <div className="m-auto text-center">
+          <h1>You Won!</h1>
+          <a onClick={() => setWon(false)}>Play Again!</a>
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <>
-      <p>testing</p>
-    </>
+    <div className="flex h-screen">
+      <div className="m-auto text-center">
+        <div style={{ backgroundColor: color }} className="p-20 border-5 rounded-md" />
+        <div className='m-4'>
+          {colors.map((color, index) => <button className='mx-2 px-2' onClick={handleOnClick} key={index}>{color}</button>)}
+        </div>
+      </div>
+    </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,41 @@
 import { Link } from 'react-router-dom'
 import reactLogo from '/react.svg'
 
+function padZero(num) {
+  if (num < 10) {
+    return `00${num}`
+  } else if (num < 100) {
+    return `0${num}`
+  } else {
+    return num.toString()
+  }
+}
+
+const challenges = [
+  { id: 'guess-the-color', name: 'Guess The Color' },
+  { id: 'debounce-input', name: 'Debounce Input' },
+]
+
+function Challenge(props) {
+  const { id, name, index } = props
+  return (
+    <a>
+      <Link to={`/${id}`}>
+        <span>{padZero(index + 1)}</span>
+        <b>{name}</b>
+      </Link>
+    </a>
+  )
+}
+
 export default function Home() {
   return (
     <div className="flex h-screen">
-      <div className="flex flex-col m-auto">
+      <div className="flex flex-col m-auto text-center">
         <img src={reactLogo} className="logo" alt="React logo" />
         <h1>Challenges</h1>
         <div id="challenges" className="grid gap-x-4 gap-y-1 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-          <Link>Guess The Color</Link>
-          <Link>Debounce Input</Link>
+          {challenges.map(({ id, name }, index) => <Challenge key={id} id={id} name={name} index={index} />)}
         </div>
       </div>
     </div>
